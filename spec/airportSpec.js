05485929e0b99d9f132b379d2503ffb600a06383
Stephen Tilley <stephentilley@gmail.com>
2018-02-20 15:19:30 +0000

    Takeoff removes a plane from the hangar, tested and passing

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -2,11 +2,19 @@ describe ('Airport', function() {
 
   var airport;
 
-  it("lands an airplane", function() {
+  it("lands airplanes", function() {
     airport = new Airport();
     airport.land('plane');
-    expect(airport.hangar).toEqual(['plane']);
+    airport.land('another plane');
+    expect(airport.hangar).toEqual(['plane', 'another plane']);
+  });
 
+  it("removes airplanes from the airport", function() {
+    airport = new Airport();
+    airport.land('plane');
+    airport.land('another plane');
+    airport.takeoff('plane')
+    expect(airport.hangar).toEqual(['another plane']);
   });
 
 });
--- src/airport.js
+++ src/airport.js
@@ -6,6 +6,11 @@ Airport.prototype.land = function(plane) {
   this.hangar.push(plane);
 };
 
+Airport.prototype.takeoff = function(plane) {
+  var index = this.hangar.indexOf(plane);
+  this.hangar.splice(index, 1);
+};
+
 var airport = new Airport();
 
 airport.land('a plane');
